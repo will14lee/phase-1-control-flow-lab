f6b41b5f37cd5cb2380eab738e359b75509e36c5
will14lee <will14lee@gmail.com>
2021-06-07 23:13:33 -0400

    test

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//const feet=300
+function scuberGreetingForFeet(feet){
+  if (feet<=400) {
+    console.log(`The distance is ${feet} feet!`);
+    return "This one is on me!";
+  }
+  else if (feet>=2500) {
+    console.log( `The distance is ${feet} feet! Leave, and never return, Simba`);
+    return "No can do.";
+  }
+  else if (feet>=2000) {
+    console.log(`The distance is ${feet} feet! PAY UP!`);
+    return "I will gladly take your thirty bucks.";
+  }
+}
+scuberGreetingForFeet(2501);
+ 
+function ternaryCheckCity(city){
+    console.log(`You going to ${city}?! Lol.`);
+    let response;
+    city=== "NYC"? (response="Ok, sounds good.") : (response='No go.');
+    return response
+}
+ternaryCheckCity("NYC");
+
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip) {
+  case "generous":
+    response= "Thank you so much.";
+    console.log(response);
+    break;
+  case "not generous":
+    response= "Thank you.";
+    console.log(response);
+    break;
+  default:
+    response= "Bye.";
+    console.log(response);
+    break;
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip("not generous");
